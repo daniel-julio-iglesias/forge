76480c3d2dafdd607fdf3da3e319acb99d4fc69c
daniel-julio-iglesias <daniel.julio.iglesias@gmail.com>
2018-10-01 23:40:03 +0300

    [UPD] forge V0.1

--- forge.py
+++ forge.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 import python_forex_quotes
+from config import Config
 
 """"Python API script: forge
 
@@ -13,6 +14,8 @@ Description: Forex currency market data
 Auth: apiKey
 HTTPS: Yes
 CORS: Unknown
+
+YOUR_API_KEY=SECRET_KEY  {environment variable}
 
 Script would be called like:
 
@@ -54,17 +57,72 @@ RESPONSE:
           timestamp: 1502160794
      }
 ]
+
+Current Plan: Free
+Update frequency: 100 updates per second
+Currency pairs: Over 700 currency pairs+gold and silver
+REST Rate Limiting: 1,000 requests per day
+WebSocket Support: None
+Delivery formats: JSON
+Redundancy: Basic
+Encryption: 256-bit
 """
 
-client = python_forex_quotes.ForexDataClient('YOUR_API_KEY')
 
-if client.marketIsOpen() == True:
-    print("Market is open!")
+class Forge:
+    def __init__(self, api_key=None):
+        # You can get an API key for free at 1forge.com
+        if api_key is None:
+            self.api_key = Config.SECRET_KEY or 'YOUR_API_KEY'
+        else:
+            self.api_key = api_key
+        # Instantiate the client
+        self.client = python_forex_quotes.ForexDataClient(self.api_key)
 
-print(client.getSymbols())
-print(client.getQuotes(['EURUSD', 'GBPJPY']))
-print(client.quota())
-print(client.convert('EUR', 'USD', 100))
+    # Check if the market is open
+    def market_is_open(self):
+        if self.client.marketIsOpen():
+            return True
+        else:
+            return False
+
+    # Get the list of available symbols
+    def get_symbols(self):
+        symbols = self.client.getSymbols()
+        return symbols
+
+    # Get quotes for specified symbols
+    def get_quotes(self, specified_symbols):
+        quotes = self.client.getQuotes(specified_symbols)
+        return quotes
+
+    # Check your usage / quota limit
+    def quota(self):
+        quota = self.client.quota()
+        return quota
+
+    # Convert from one currency to another
+    def convert(self, from_currency, to_currency, from_currency_value):
+        conversion = self.client.convert(from_currency, to_currency, from_currency_value)
+        return conversion
+
 
 if __name__ == '__main__':
-    pass
+    forge = Forge()
+
+    if forge.market_is_open():
+        print("Market is open!")
+    else:
+        print("Market is closed!")
+
+    print(forge.get_symbols())
+
+    specified_symbols = ['EURUSD', 'GBPJPY']
+    print(forge.get_quotes(specified_symbols))
+
+    print(forge.quota())
+
+    from_currency = 'EUR'
+    to_currency = 'USD'
+    from_currency_value = 100
+    print(forge.convert(from_currency, to_currency, from_currency_value))
