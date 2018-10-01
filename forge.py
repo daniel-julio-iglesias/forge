9e83eaf8d966377192f63f21f469ad88ff85733e
daniel-julio-iglesias <daniel.julio.iglesias@gmail.com>
2018-10-01 21:27:07 +0300

    [UPD] forge V0.1

--- forge.py
+++ forge.py
@@ -3,7 +3,13 @@
 
 import python_forex_quotes
 
-#You can get an API key for free at 1forge.com
 client = python_forex_quotes.ForexDataClient('YOUR_API_KEY')
 
+if client.marketIsOpen() == True:
+    print "Market is open!"
+
+print client.getSymbols()
+print client.getQuotes(['EURUSD', 'GBPJPY'])
+print client.quota()
+print client.convert('EUR', 'USD', 100)
 
