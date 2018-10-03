d1e7872b8705cf2cfca7099e9d9f0c8b85310817
Daniel Julio Iglesias Hambálek <daniel.julio.iglesias@gmail.com>
2018-10-03 14:49:06 +0300

    [UPD] forge v1.0

--- config.py
+++ config.py
@@ -8,6 +8,7 @@ class Config:
     def __init__(self):
         # You can get an API key for free at 1forge.com
         self.FORGE_API_KEY = os.environ.get('FORGE_API_KEY') or 'YOUR_API_KEY'
+
         # In case you receive an Error Message as
         # "(...)IOError: [Errno socket error] [Errno 10060] A connection attempt failed because the
         # connected party did not properly respond after a period of time, or established connection
@@ -15,7 +16,7 @@ class Config:
         # and you are behind a proxy
         # adapt the next environment variables
         # os.environ['http_proxy'] = 'http://username:password@Proxyadresse:Proxyport'
-        # os.environ['http_proxy'] = 'https://username:password@Proxyadresse:Proxyport'
+        # os.environ['https_proxy'] = 'https://username:password@Proxyadresse:Proxyport'
 
 
 if __name__ == '__main__':
